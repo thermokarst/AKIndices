cdb1ccf6d67c1339676369ea5318b5d87bc4b78e
Matthew Dillon <mrdillon@alaska.edu>
2015-10-10 15:00:08 -0700

    JK, Procfile

--- manage.py
+++ manage.py
@@ -1,29 +1,7 @@
 #!/usr/bin/env python
 import os
 from app import create_app, db
-from flask.ext.script import Manager, Shell, Command, Option
-
-
-# http://stackoverflow.com/a/24606817
-class GunicornServer(Command):
-    """Run the app within Gunicorn"""
-
-    def get_options(self):
-        from gunicorn.config import make_settings
-
-        settings = make_settings()
-        options = (
-            Option(*klass.cli, action=klass.action)
-            for setting, klass in settings.iteritems() if klass.cli
-        )
-        return options
-
-    def run(self, *args, **kwargs):
-        from gunicorn.app.wsgiapp import WSGIApplication
-
-        app = WSGIApplication()
-        app.app_uri = 'manage:app'
-        return app.run()
+from flask.ext.script import Manager
 
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -45,8 +23,6 @@ def initdb():
             CONSTRAINT new_communities_pkey PRIMARY KEY (id)
         );"""
     _ = db.engine.execute(text(cmd))
-
-manager.add_command("gunicorn", GunicornServer())
 
 
 if __name__ == '__main__':
